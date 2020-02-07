8fa695ba3373c06f7d8f1768db277c1f63161c22
SJ Kim <sjkim0421@gmail.com>
2020-02-07 16:43:07 -0500

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  # Allows sitewide basic auth to be enabled via environment variables.
+  def basic_auth
+    if ENV['BASIC_AUTH_ACTIVATED'] && ENV['BASIC_AUTH_ACTIVATED'].downcase == "true"
+      authenticate_or_request_with_http_basic('Dev Access') do |username, password|
+        username == ENV['BASIC_AUTH_USERNAME']
+        password == ENV['BASIC_AUTH_PASSWORD']
+      end
+    end
+  end
 end
