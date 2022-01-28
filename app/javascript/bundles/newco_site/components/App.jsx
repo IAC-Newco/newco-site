59c6f866df51d6970ee176143ec72e17443f139d
Sam Kim <sam@buildwithnewco.com>
2022-01-28 10:37:47 -0500

    start on jobvite iframe

--- app/javascript/bundles/newco_site/components/App.jsx
+++ app/javascript/bundles/newco_site/components/App.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import Home from "./newco_site";
 import CareersPage from "./CareersPage/careers_page";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/careers" element={<CareersPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <HelmetProvider>
+      <div className="app">
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/careers" element={<CareersPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </HelmetProvider>
   );
 }
 
--- app/javascript/bundles/newco_site/components/CareersPage/careers_page.jsx
+++ app/javascript/bundles/newco_site/components/CareersPage/careers_page.jsx
@@ -2,22 +2,35 @@ import React, { useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import styles from "./CareersPage.module.scss";
+import { Helmet } from "react-helmet-async";
 
 function CareersPage() {
   useEffect(() => {
     document.body.style.backgroundColor = "white";
   }, []);
+  // useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src =
+  //     "//jobs.jobvite.com/__assets__/scripts/careersite/public/iframe.js";
+  //   script.async = true;
+  //   document.body.appendChild(script);
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
   return (
     <div className={styles.wrapper}>
+      <Helmet>
+        <script src="//jobs.jobvite.com/__assets__/scripts/careersite/public/iframe.js"></script>
+      </Helmet>
       <Header />
       <div className={styles.content}>
         <div
-          class="jv-careersite"
-          data-careersite="newco"
+          className="jv-careersite"
+          data-careersite="jobs.jobvite.com/newco"
           jobvite-iframeParameter="true"
           data-force-redirect
         ></div>
-        <script src="//jobs.jobvite.com/__assets__/scripts/careersite/public/iframe.js"></script>
       </div>
       <Footer />
     </div>
