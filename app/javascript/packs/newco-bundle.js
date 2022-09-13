edde51ae822b68a496f2795c896f3a28bd715d51
Samuel Kim <sam@buildwithnewco.com>
2022-09-13 16:21:04 -0400

    remove hello world

--- app/javascript/packs/newco-bundle.js
+++ app/javascript/packs/newco-bundle.js
@@ -2,7 +2,6 @@ import ReactOnRails from "react-on-rails";
 
 import App from "../bundles/newco_site/components/App";
 
-// This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   App
 });
