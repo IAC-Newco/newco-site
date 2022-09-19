a0295e8b683a6144698b07b5bccc61b77b9574cc
Samuel Kim <sam@buildwithnewco.com>
2022-09-19 14:33:50 -0400

    lazy load careers page

--- app/javascript/bundles/newco_site/components/App.jsx
+++ app/javascript/bundles/newco_site/components/App.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Home from "./newco_site";
-import CareersPage from "./CareersPage/careers_page";
+// import CareersPage from "./CareersPage/careers_page";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 function App() {
+  const CareersPage = React.lazy(() => import("./CareersPage/careers_page"));
+
   return (
     <HelmetProvider>
       <div className="app">
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/careers" element={<CareersPage />} />
-          </Routes>
-        </Router>
+        <Suspense>
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/careers" element={<CareersPage />} />
+            </Routes>
+          </Router>
+        </Suspense>
       </div>
     </HelmetProvider>
   );
